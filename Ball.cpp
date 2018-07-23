2a80c69f0061043c34b78b902689ef1e2372b4d1
Arsyad bin Anuar <syaads@yahoo.com.my>
2018-07-23 19:56:21 +0800

    Update Ball.cpp

--- Ball.cpp
+++ Ball.cpp
@@ -1,10 +1,3 @@
-/********************************************
-Course : TGD2251 Game Physics
-Session: Trimester 2, 2016/17
-ID and Name #1 : 1142700937 Arsyad bin Anuar
-Contacts #1 : 019-3456645 rsyad126@gmail.com
-********************************************/
-
 #include "stdafx.h"
 #include "Ball.h"
 
